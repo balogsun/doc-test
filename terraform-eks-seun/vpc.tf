b9feca46294ff250d2bfd4bf1cc9af94c12b8a14
SeunB <125329091+balogsun@users.noreply.github.com>
2024-05-26 17:57:29 -0400

    Update vpc.tf

--- terraform-eks-seun/vpc.tf
+++ terraform-eks-seun/vpc.tf
@@ -1,3 +1,4 @@
+# Create VPC
 resource "aws_vpc" "pjct-vpc" {
   cidr_block = "10.0.0.0/16"
 
@@ -7,6 +8,7 @@ resource "aws_vpc" "pjct-vpc" {
   })
 }
 
+# Create public subnets
 resource "aws_subnet" "pjct-public-subnet" {
   count = 3
 
@@ -21,6 +23,7 @@ resource "aws_subnet" "pjct-public-subnet" {
   })
 }
 
+# Create private subnets
 resource "aws_subnet" "pjct-private-subnet" {
   count = 3
 
@@ -34,6 +37,7 @@ resource "aws_subnet" "pjct-private-subnet" {
   })
 }
 
+# Create Internet Gateway
 resource "aws_internet_gateway" "pjct-vpc" {
   vpc_id = aws_vpc.pjct-vpc.id
 
@@ -42,6 +46,7 @@ resource "aws_internet_gateway" "pjct-vpc" {
   }
 }
 
+# Create "route table" [plan it for public subnets] with a "route" to the internet gateway
 resource "aws_route_table" "pjct-public-rt" {
   vpc_id = aws_vpc.pjct-vpc.id
 
@@ -55,6 +60,36 @@ resource "aws_route_table" "pjct-public-rt" {
   }
 }
 
+# Create Elastic IP for the NAT Gateway
+resource "aws_eip" "nat-gateway-eip" {
+  domain = "vpc"
+}
+
+# Create NAT Gateway, allocate the elastic IP to it, Place the NAT in the first public subnet
+resource "aws_nat_gateway" "nat-gateway" {
+  allocation_id = aws_eip.nat-gateway-eip.id
+  subnet_id     = aws_subnet.pjct-public-subnet[0].id
+
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+# Create a "route table" [plan it for private subnets] with a "route" to the NAT Gateway
+resource "aws_route_table" "pjct-private-rt" {
+  vpc_id = aws_vpc.pjct-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id         = aws_nat_gateway.nat-gateway.id
+  }
+
+  tags = {
+    Name = "pjct-private-rt"
+  }
+}
+
+# Associate the public route table with public subnets
 resource "aws_route_table_association" "pjct-public-rt-assoc" {
   count = 3
 
@@ -62,42 +97,10 @@ resource "aws_route_table_association" "pjct-public-rt-assoc" {
   route_table_id = aws_route_table.pjct-public-rt.id
 }
 
-resource "aws_route_table" "pjct-private-rt" {
-  count = 3
-  vpc_id = aws_vpc.pjct-vpc.id
-
-  tags = {
-    Name = "pjct-private-rt-${count.index}"
-  }
-}
-
+# Associate the private route table with each private subnet.
 resource "aws_route_table_association" "pjct-private-rt-assoc" {
   count = 3
 
   subnet_id      = aws_subnet.pjct-private-subnet[count.index].id
-  route_table_id = aws_route_table.pjct-private-rt[count.index].id
+  route_table_id = aws_route_table.pjct-private-rt.id
 }
-
-#resource "aws_eip" "nat-gateway-eip" {
-#  count  = 3
-#  domain = "vpc"
-#}
-#
-#resource "aws_nat_gateway" "nat-gateway" {
-#  count = 3
-#
-#  allocation_id = aws_eip.nat-gateway-eip[count.index].id
-#  subnet_id     = aws_subnet.pjct-public-subnet[count.index].id
-#
-#  tags = {
-#    Name = "nat-gateway-${count.index}"
-#  }
-#}
-#
-#resource "aws_route" "nat-gateway-route" {
-#  count = 3
-#
-#  route_table_id         = aws_route_table.pjct-private-rt[count.index].id
-#  destination_cidr_block = "0.0.0.0/0"
-#  nat_gateway_id         = aws_nat_gateway.nat-gateway[count.index].id
-#}
